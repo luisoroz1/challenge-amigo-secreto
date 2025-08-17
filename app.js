6a25a32f7132c8e280b21d0ee70f33197d24e19b
Luis Orozco <luisoroz981@gmail.com>
2025-08-16 22:17:54 -0600

    Se agregó un mensaje de prueba a app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
+console.log("¡Hola mundo! Esto es una prueba.");
